652c035c9bda89c455e52fa5405c942b85a0d01a
Evgeniy <vrd096@mail.ru>
2019-12-03 21:40:03 +0500

    рефакторинг кода

--- js/form.js
+++ js/form.js
@@ -5,13 +5,24 @@
   let modelOverlay = document.querySelector(".modal-overlay");
   let contactButton = document.querySelector(".contacts__button");
   let modalClose = document.querySelector(".modal-send__close");
+  const ESC_KEYCODE = 27;
 
-  contactButton.addEventListener("click", function(){
-    modelSend.classList.remove("visually-hidden");
-    modelOverlay.classList.remove("visually-hidden");
-  })
-  modalClose.addEventListener("click", function(){
+  function closeModal (){
     modelSend.classList.add("visually-hidden");
     modelOverlay.classList.add("visually-hidden");
-  });
+  }
+
+  function showModal() {
+    modelSend.classList.remove("visually-hidden");
+    modelOverlay.classList.remove("visually-hidden");
+  }
+
+  function handleCardEsc({ keyCode }) {
+    if (keyCode === ESC_KEYCODE) {
+      closeModal();
+    }
+  }
+
+  contactButton.addEventListener("click", showModal);
+  modalClose.addEventListener("click", closeModal);
 })();
