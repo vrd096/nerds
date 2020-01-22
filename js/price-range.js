b70e4cb791fe62c1feebe12c6f10c0663b65cddd
Evgeniy <vrd096@mail.ru>
2020-01-22 22:10:40 +0500

    добавил кнопку для проверки слайдера

--- js/price-range.js
+++ js/price-range.js
@@ -1,16 +1,22 @@
 "use strict";
 
 (function(){
-  var slider = document.getElementById('r-slider');
+  let slider = document.getElementById('r-slider');
 
   noUiSlider.create(slider, {
-      start: [10, 80],
+      start: [10, 15000],
       connect: true,
+      step: 10,
       range: {
           'min': 0,
-          'max': 100
-      }
+          'max': 20000
+      },
+
+  });
+  const sliderButton = document.querySelector(".slider-btn-js");
+
+  sliderButton.addEventListener("click", function(){
+    console.log(slider.noUiSlider.get());
   });
 
-  console.log(slider.noUiSlider.get());
 }());
