79feed07f0e9368d92b231e7cfdc7b78021da12b
Evgeniy <vrd096@mail.ru>
2020-01-22 23:16:44 +0500

    Сделал чтобы в инпутах отражались координаты слайдера, так же выставлялись координаты если их задать в инпутах

--- js/price-range.js
+++ js/price-range.js
@@ -11,12 +11,43 @@
           'min': 0,
           'max': 20000
       },
-
+      format: {
+        // 'to' the formatted value. Receives a number.
+        to: function (value) {
+            return Math.round(value) ;
+        },
+        // 'from' the formatted value.
+        // Receives a string, should return a number.
+        from: function (value) {
+            return Number(value.replace('', ''));
+        }
+    }
   });
+
+let sliderItemLeft = document.getElementById('slider-item-left');
+let sliderItemRight = document.getElementById('slider-item-right');
+
+slider.noUiSlider.on('update', function (values, handle) {
+  let sliderCoords = slider.noUiSlider.get();
+    sliderItemLeft.value = sliderCoords[0];
+});
+slider.noUiSlider.on('update', function (values, handle) {
+  let sliderCoords = slider.noUiSlider.get();
+    sliderItemRight.value = sliderCoords[1];
+});
+
+sliderItemLeft.addEventListener('change', function () {
+  slider.noUiSlider.set([this.value, null]);
+});
+sliderItemRight.addEventListener('change', function () {
+  slider.noUiSlider.set([null, this.value]);
+});
+
   const sliderButton = document.querySelector(".slider-btn-js");
 
   sliderButton.addEventListener("click", function(){
-    console.log(slider.noUiSlider.get());
+    let sliderCoords = slider.noUiSlider.get();
+    console.log(sliderCoords[0]);
   });
 
 }());
