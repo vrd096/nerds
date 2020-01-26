a701b137a28da2862227ae5ace393972ee864ffc
Evgeniy <vrd096@mail.ru>
2020-01-26 19:35:05 +0500

    накидана идея

---
+++ js/template-model.js
@@ -0,0 +1,103 @@
+"use strict";
+
+const ADAPTIVE_TYPE = "adaptive";
+const SLIDER = "slider";
+const NEWS = "news";
+const BENEFITS = "BENEFITS";
+
+(function() {
+  let items = [
+    {
+      title: "B",
+      description: "ЧТо-то",
+      price: 2,
+      picture: "aplus.jpg",
+      type: ADAPTIVE_TYPE,
+      features: [NEWS],
+
+      addFeature(feature) {
+        this.features.push(feature);
+      },
+
+      get hasBenefit() {
+        return this.features.includes(BENEFITS);
+      }
+    },
+    {
+      title: "Cllkj",
+      description: "ЧТо-то",
+      price: 1,
+      picture: "aplus.jpg",
+      type: ADAPTIVE_TYPE,
+      features: [NEWS]
+    },
+    {
+      title: "A",
+      description: "ЧТо-то",
+      price: 3,
+      picture: "aplus.jpg",
+      type: ADAPTIVE_TYPE,
+      features: [SLIDER, NEWS]
+    }
+  ];
+
+  console.log(items.map(({title}) => title));
+
+  // sort by type, price, title
+  // let sortedItems = [...items].sort(({ title: a}, {title: b}) => a.localeCompare(b));
+  // let sortedItems = [...items].sort((itemA, itemB) => itemA.title.localeCompare(itemB.title));
+  // let sortedItems = [...items].sort((itemA, itemB) => itemA.price - itemB.price);
+
+  let requiredFeatures = [SLIDER, NEWS];
+
+  // let filteredItems = items.filter((item) => requiredFeatures.every(required => item.features.includes(required)));
+
+  // let filteredItems = items.filter((item) => {
+  //   for (let req of requiredFeatures) {
+  //     if (!item.features.includes(req)) {
+  //       return false;
+  //     }
+  //   }
+
+  //   return true;
+  // });
+
+  let filteredItems = items.filter((item) => {
+    for (let req of requiredFeatures) {
+      if (!item.features.includes(req)) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+
+  let item1 = items[0];
+
+  console.log(item1.hasBenefit)
+  item1.addFeature(BENEFITS);
+  console.log(item1.hasBenefit)
+
+  console.log(filteredItems.map(({title}) => title));
+
+
+  let template = `
+  <li class="catalog__templates-item">
+    <div class="catalog__template-header">
+      <span class="catalog__template-header-circle"></span>
+    </div>
+    <img
+      src="img/pink.jpg"
+      alt=""
+      class="catalog__template-image"
+    />
+    <div class="catalog__template-price">
+      <p class="catalog__template-price-title">${items[0].title}</p>
+      <p class="catalog__template-price-description">${items[0].description}</p>
+      <button class="catalog__template-price-button button" type="button">9 900 Руб.</button>
+    </div>
+  </li>
+  `
+
+  console.log(template);
+})();
